d3df0e212bba88c7e9714ebe9a437090b452dbd1
JoelTchouke <tchoukejoel@gmail.com>
2026-01-27 19:33:56 -0600

    Time: 0 ms (100.00%) | Memory: 16.5 MB (15.43%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,32 +1,30 @@
 using namespace std;
 class Solution {
 public:
-    int m, n;
-    vector<int> memo;
-    static long long key(int i, int j)
+    int minPathSum(vector<vector<int>>& grid) 
     {
-        return ((long long) i << 32 | (unsigned int) j);
-    }
-    int minimumPathToReach(int i, int j, vector<vector<int>>& grid)
-    {
-        if (i < 0 || j < 0) return INT_MAX;   // INF (use /2 to avoid overflow)
-        if(i == 0 && j == 0)
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<vector<int>> costMap(m, vector<int>(n, 0));
+        costMap[0][0] = grid[0][0];
+        for(int i = 1; i < m; i++)
         {
-            return grid[0][0];
+            costMap[i][0] = costMap[i - 1][0] + grid[i][0];
         }
-        int index = i * n + j;
-        if(memo[index] != -1)
+        for(int j = 1; j < n; j++)
         {
-            return memo[index];
+            costMap[0][j] = costMap[0][j - 1] + grid[0][j];
         }
-        int answer = grid[i][j] + min(minimumPathToReach(i - 1, j, grid), minimumPathToReach(i, j - 1, grid));
-        
-        return memo[index] = answer;
-    }
-    int minPathSum(vector<vector<int>>& grid) {
-        m = grid.size();
-        n = grid[0].size();
-        memo.assign(m * n, -1);
-        return minimumPathToReach(m - 1, n - 1, grid);
+
+        for(int i = 1; i < m; i++)
+        {
+            for(int j = 1; j < n; j++)
+            {
+                costMap[i][j] = grid[i][j] + min(costMap[i - 1][j], costMap[i][j - 1]);
+            }
+        }
+        return costMap[m - 1][n - 1];
+
     }
 };
