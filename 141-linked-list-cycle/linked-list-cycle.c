33d2de5bd0e049ceee7a333ee65e376e66520caa
JoelTchouke <tchoukejoel@gmail.com>
2026-02-05 20:46:19 -0600

    Time: 13 ms (38.17%) | Memory: 11.4 MB (19.38%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.c
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+bool hasCycle(struct ListNode *head) {
+    if(head == NULL) return false;
+    struct ListNode* tortoise = head;
+    struct ListNode* hare = head -> next;
+    bool cycleFound = false;
+    while(hare != NULL && hare -> next != NULL)
+    {
+        tortoise = tortoise -> next;
+        hare = hare -> next -> next;
+        if(tortoise == hare)
+        {
+            cycleFound = true;
+            break;
+        }
+
+    }
+
+    return cycleFound;
+}
