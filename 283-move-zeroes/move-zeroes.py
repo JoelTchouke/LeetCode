f4bc2a934242caa7ae388072e46d536f1c3fcae5
JoelTchouke <tchoukejoel@gmail.com>
2026-02-15 22:29:12 -0600

    Time: 379 ms (9.41%) | Memory: 13.4 MB (86.97%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -4,15 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        arrOfZero = []
-        arrOfNonZero = []
-        for num in nums:
-            if num != 0:
-                arrOfNonZero.append(num)
-            else:
-                arrOfZero.append(num)
         count = nums.count(0)
-        while count > 0:
+        while(count > 0):
             nums.remove(0)
+            nums.append(0)
             count -= 1
-        nums = nums.extend(arrOfZero)
