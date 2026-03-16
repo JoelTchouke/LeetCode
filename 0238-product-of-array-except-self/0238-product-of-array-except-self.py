120bff123998bad49765642da2c049a58139cdff
JoelTchouke <tchoukejoel@gmail.com>
2026-03-15 22:13:21 -0500

    Time: 26 ms (93.61%), Space: 19.3 MB (97.09%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        zero_count = nums.count(0)
+        multiplication = 1
+        res = []
+
+        for num in nums:
+            if num != 0:
+                multiplication *= num
+
+        for num in nums:
+            if zero_count > 1:
+                res.append(0)
+            elif zero_count == 1:
+                if num == 0:
+                    res.append(multiplication)
+                else:
+                    res.append(0)
+            else:
+                res.append(multiplication // num)
+
+        return res
