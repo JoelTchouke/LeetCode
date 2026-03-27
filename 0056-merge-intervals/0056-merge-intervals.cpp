bfe8f43df452b7431826235b1dc72e9c0c0ca868
JoelTchouke <tchoukejoel@gmail.com>
2026-03-26 23:58:27 -0500

    Time: 12 ms (9.66%), Space: 26 MB (5.16%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        std::sort(intervals.begin(), intervals.end()); 
+        vector<vector<int>> res = {intervals[0]};
+        for(vector<int> interval : intervals)
+        {
+            vector<int> arr = res.back();
+            if(arr[1] >= interval[0] && arr[1] <= interval[1])
+            {
+                res.back() = {arr[0], interval[1]};
+            }
+            else if(arr[0] <= interval[0] && arr[1] >= interval[1])
+            {
+                res.back() = arr;
+            }
+            else
+            {
+                res.push_back(interval);
+            }
+        }
+        return res;
+    }
+};
