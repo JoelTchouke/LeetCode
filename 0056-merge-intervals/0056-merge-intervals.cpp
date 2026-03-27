82cf439bc55b0086702b770bdee21028d5e3e1d1
JoelTchouke <tchoukejoel@gmail.com>
2026-03-27 00:05:36 -0500

    Time: 9 ms (17.78%), Space: 25.9 MB (5.16%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -5,14 +5,20 @@ public:
         std::sort(intervals.begin(), intervals.end()); 
         vector<vector<int>> res;
         res.push_back(intervals[0]);
-        for(int i = 1; i < intervals.size(); i++)
+        for(vector<int> interval : intervals)
         {
-            if (intervals[i][0] <= res.back()[1]) 
+            vector<int> arr = res.back();
+            if(arr[1] >= interval[0] && arr[1] <= interval[1])
             {
-                res.back()[1] = max(res.back()[1], intervals[i][1]);
-            } else 
+                res.back() = {arr[0], interval[1]};
+            }
+            else if(arr[0] <= interval[0] && arr[1] >= interval[1])
             {
-                res.push_back(intervals[i]);
+                res.back() = arr;
+            }
+            else
+            {
+                res.push_back(interval);
             }
         }
         return res;
